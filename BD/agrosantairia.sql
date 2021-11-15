2228c6321f43f2b26c0f733d55ef5dd925a705a6
Larissa Leal <larissa.leal@ufms.br>
2021-11-14 20:34:47 -0400

    dump database

--- BD/agrosantairia.sql
+++ BD/agrosantairia.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 14/02/2020 às 22:13
--- Versão do servidor: 10.4.11-MariaDB
--- Versão do PHP: 7.4.1
+-- Tempo de geração: 15/11/2021 às 01:33
+-- Versão do servidor: 10.4.20-MariaDB
+-- Versão do PHP: 7.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `agrosantairia`
 --
+CREATE DATABASE IF NOT EXISTS `agrosantairia` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `agrosantairia`;
 
 -- --------------------------------------------------------
 
@@ -38,16 +39,13 @@ CREATE TABLE `clientes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Tabela truncada antes do insert `clientes`
---
-
-TRUNCATE TABLE `clientes`;
---
 -- Despejando dados para a tabela `clientes`
 --
 
 INSERT INTO `clientes` (`IDCliente`, `Nome`, `DataNasc`, `Email`, `Telefone`) VALUES
-(0, 'Anônimo', '', '', '');
+(0, 'Anônimo', '', '', ''),
+(11, 'Nome Cliente', '2022-02-22', 'email@gmail.com', '(67) 99283-7982'),
+(12, 'Larissa', '1998-04-27', 'email@gmail.com', '(67) 98889-8989');
 
 -- --------------------------------------------------------
 
@@ -80,17 +78,34 @@ CREATE TABLE `login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Tabela truncada antes do insert `login`
---
-
-TRUNCATE TABLE `login`;
---
 -- Despejando dados para a tabela `login`
 --
 
 INSERT INTO `login` (`IDLogin`, `user`, `password`, `IDGroup`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 2),
-(2, 'igor', 'dd97813dd40be87559aaefed642c3fbb', 1);
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `IDVenda` int(11) NOT NULL,
+  `IDProduto` int(11) NOT NULL,
+  `acao` varchar(20) NOT NULL,
+  `data` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `logs`
+--
+
+INSERT INTO `logs` (`id`, `IDVenda`, `IDProduto`, `acao`, `data`) VALUES
+(4, 11, 0, 'INSERT VENDA', '2021-11-14 20:22:50'),
+(5, 0, 8, 'INSERT PRODUTO', '2021-11-14 20:32:05');
 
 -- --------------------------------------------------------
 
@@ -103,6 +118,22 @@ CREATE TABLE `produtos` (
   `IDProduto` int(11) NOT NULL,
   `Nome` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `produtos`
+--
+
+INSERT INTO `produtos` (`IDProduto`, `Nome`) VALUES
+(8, 'ui');
+
+--
+-- Gatilhos `produtos`
+--
+DROP TRIGGER IF EXISTS `insertProduto`;
+DELIMITER $$
+CREATE TRIGGER `insertProduto` AFTER INSERT ON `produtos` FOR EACH ROW INSERT INTO logs VALUES(null, 0, NEW.IDProduto, 'INSERT PRODUTO', NOW())
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -120,6 +151,33 @@ CREATE TABLE `vendas` (
   `IDCliente` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Despejando dados para a tabela `vendas`
+--
+
+INSERT INTO `vendas` (`IDVenda`, `Produto`, `Quantidade`, `Valor`, `Total`, `IDCliente`) VALUES
+(4, 'Feijão', 3, '23.00', '69.00', 11),
+(11, 'Feijão', 2, '7.00', '14.00', 12);
+
+--
+-- Gatilhos `vendas`
+--
+DROP TRIGGER IF EXISTS `deleteVenda`;
+DELIMITER $$
+CREATE TRIGGER `deleteVenda` BEFORE DELETE ON `vendas` FOR EACH ROW INSERT INTO logs VALUES(null, OLD.IDVenda, "DELETE VENDA", NOW())
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `insertVenda`;
+DELIMITER $$
+CREATE TRIGGER `insertVenda` AFTER INSERT ON `vendas` FOR EACH ROW INSERT INTO logs VALUES(null, NEW.IDVenda, 'INSERT VENDA', NOW())
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `updateVenda`;
+DELIMITER $$
+CREATE TRIGGER `updateVenda` AFTER UPDATE ON `vendas` FOR EACH ROW INSERT INTO logs VALUES(null, NEW.IDVenda, 'UPDATE VENDA', NOW())
+$$
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -134,7 +192,14 @@ CREATE TABLE `visitas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices de tabelas apagadas
+-- Despejando dados para a tabela `visitas`
+--
+
+INSERT INTO `visitas` (`IDVisita`, `Visitas_agrosantairia`, `Data`) VALUES
+(1, 6, '2021-11-14 22:57:10');
+
+--
+-- Índices para tabelas despejadas
 --
 
 --
@@ -156,6 +221,12 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`IDLogin`);
 
 --
+-- Índices de tabela `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices de tabela `produtos`
 --
 ALTER TABLE `produtos`
@@ -175,14 +246,14 @@ ALTER TABLE `visitas`
   ADD PRIMARY KEY (`IDVisita`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `IDCliente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `IDCliente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `interessado`
@@ -197,25 +268,31 @@ ALTER TABLE `login`
   MODIFY `IDLogin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de tabela `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `IDProduto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDProduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `vendas`
 --
 ALTER TABLE `vendas`
-  MODIFY `IDVenda` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDVenda` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de tabela `visitas`
 --
 ALTER TABLE `visitas`
-  MODIFY `IDVisita` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDVisita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Restrições para dumps de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
